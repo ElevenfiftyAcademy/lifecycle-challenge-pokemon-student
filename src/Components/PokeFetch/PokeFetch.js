722a5794bf3d8e82bde77a3e3c07c98b9c29fbf4
Danielle <dlgeiser2992@gmail.com>
2021-03-18 11:39:20 -0400

    first commit

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -2,50 +2,17 @@ import React, { Component } from 'react'
 import './PokeFetch.css';
 
 
-
 class PokeFetch extends Component {
   constructor() {
     super()
     this.state = {
-      pokeURL: '',
+      pokeInfo: '',
       pokeSprite: '',
       pokeName: '',
-      isRunning: false,
-      seconds: 10,
-      isVisible: false
     }
   }
-
-  countDown() {
-    if (this.state.isRunning === true && this.state.seconds > 0) {
-      this.setState((prevState) => ({
-        seconds: prevState.seconds - 1
-      }))
-    }
-  };
-
-  componentDidMount() {
-    this.interval = setInterval(() => this.countDown(), 1000);
-    console.log(this.interval);
-  }
-
-  componentDidUpdate() {
-    if (this.state.seconds === 0 && this.state.isVisible === false) {
-      this.setState({ isVisible: true })
-    }
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
 
   fetchPokemon() {
-    this.setState({
-      isRunning: false,
-      isVisible: false,
-      seconds: 10
-    })
     let min = Math.ceil(1);
     let max = Math.floor(152);
     let pokeNum = Math.floor(Math.random() * (max - min) + min);
@@ -54,10 +21,9 @@ class PokeFetch extends Component {
     }).then(res => res.json())
       .then(res => {
         this.setState({
-          pokeURL: res,
+          pokeInfo: res,
           pokeSprite: res.sprites.front_default,
           pokeName: res.species.name,
-          isRunning: true
         })
       })
       .catch((err) => console.log(err))
@@ -67,13 +33,10 @@ class PokeFetch extends Component {
     return (
       <div className={'wrapper'}>
         <button className={'start'} onClick={() => this.fetchPokemon()}>Start!</button>
-        <br />
-        <h1 className={'timer'} >{this.state.seconds}</h1>
+        <h1 className={'timer'} >Timer Display</h1>
         <div className={'pokeWrap'}>
-          {this.state.isVisible ?
-            <img className={'pokeImg'} src={this.state.pokeSprite} />
-            : <img className={'pokeImg'} style={{ filter: "brightness(0)" }} src={this.state.pokeSprite} />}
-          {this.state.isVisible ? <h1 className={'pokeName'}>{this.state.pokeName}</h1> : null}
+          <img className={'pokeImg'} src={this.state.pokeSprite} />
+          <h1 className={'pokeName'}>{this.state.pokeName}</h1>
         </div>
       </div>
     )
